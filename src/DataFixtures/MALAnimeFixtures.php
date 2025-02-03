c1692f9f9a3786307f6ef8ec36e7bdcebe48b84f
ayoug <ayoub.essalhia@gmail.com>
2025-02-03 17:29:11 +0100

    feat: projet version 1

--- src/DataFixtures/MALAnimeFixtures.php
+++ src/DataFixtures/MALAnimeFixtures.php
@@ -25,10 +25,10 @@ class MALAnimeFixtures extends Fixture
     public function __construct(HttpClientInterface $httpClient, ParameterBagInterface $parameterBag)
     {
         $this->httpClient = $httpClient;
-        $this->url = "https://api.myanimelist.net/v2";
-        $this->clientId = "fea9448bfcf02ed2c536462766165325";
-        // $this->url = $parameterBag->get('MAL_API_URL'); TODO("make env work")
-        // $this->clientId = $parameterBag->get('MAL_CLIENT_ID'); TODO("make env work")
+//        $this->url = "https://api.myanimelist.net/v2";
+//        $this->clientId = "fea9448bfcf02ed2c536462766165325";
+         $this->url = $parameterBag->get('MAL_API_URL');
+         $this->clientId = $parameterBag->get('MAL_CLIENT_ID');
     }
 
     /**
@@ -40,7 +40,7 @@ class MALAnimeFixtures extends Fixture
         for ($i = 0; $i < 10; ++$i){
             // as int
             $offset = intval($i * 10);
-            $requestUrl = $this->url . '/anime/ranking?offset=' . $offset;
+            $requestUrl = $this->url . 'anime/ranking?offset=' . $offset;
             $response = $this->httpClient->request('GET', $requestUrl, [
                 'headers' => [
                     'X-MAL-Client-ID' => $this->clientId,
@@ -52,11 +52,11 @@ class MALAnimeFixtures extends Fixture
                 foreach ($data as $anime) {
                     $manager->persist((new Anime())
                         ->setTitle($anime['node']['title'])
-                        ->setPicture($anime['node']['main_picture']['medium']));
+                        ->setPicture($anime['node']['main_picture']['large']));
                 }
                 $manager->flush();
             } catch (Exception $e) {
-                throw new Exception('Error while fetching data from the API');
+                throw new Exception($response->getContent());
             }
         }
     }
--- src/Entity/Anime.php
+++ src/Entity/Anime.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\AnimeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AnimeRepository::class)]
@@ -19,6 +21,25 @@ class Anime
 
     #[ORM\Column(length: 255)]
     private ?string $picture = null;
+
+    /**
+     * @var Collection<int, User>
+     */
+    #[ORM\ManyToMany(targetEntity: User::class, mappedBy: 'likedAnimes')]
+    #[ORM\JoinTable(name: 'users_liked_animes')]
+    private Collection $usersLiking;
+
+    /**
+     * @var Collection<int, StarredAnime>
+     */
+    #[ORM\OneToMany(targetEntity: StarredAnime::class, mappedBy: 'anime', orphanRemoval: true)]
+    private Collection $userStars;
+
+    public function __construct()
+    {
+        $this->usersLiking = new ArrayCollection();
+        $this->userStars = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -45,6 +66,63 @@ class Anime
     public function setPicture(string $picture): static
     {
         $this->picture = $picture;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getUsersLiking(): Collection
+    {
+        return $this->usersLiking;
+    }
+
+    public function addUsersLiking(User $usersLiking): static
+    {
+        if (!$this->usersLiking->contains($usersLiking)) {
+            $this->usersLiking->add($usersLiking);
+            $usersLiking->addLikedAnime($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUsersLiking(User $usersLiking): static
+    {
+        if ($this->usersLiking->removeElement($usersLiking)) {
+            $usersLiking->removeLikedAnime($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, StarredAnime>
+     */
+    public function getUserStars(): Collection
+    {
+        return $this->userStars;
+    }
+
+    public function addUserStar(StarredAnime $userStar): static
+    {
+        if (!$this->userStars->contains($userStar)) {
+            $this->userStars->add($userStar);
+            $userStar->setAnime($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUserStar(StarredAnime $userStar): static
+    {
+        if ($this->userStars->removeElement($userStar)) {
+            // set the owning side to null (unless already changed)
+            if ($userStar->getAnime() === $this) {
+                $userStar->setAnime(null);
+            }
+        }
 
         return $this;
     }
