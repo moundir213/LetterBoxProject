e41773aceb6c8a441412a9a28844dd9661a67851
Michaël Ditlecadet <115543356+MichaelDitlecadet@users.noreply.github.com>
2025-02-09 22:00:06 +0100

    Implement user authentication with registration, CSRF protection, and email confirmation (WIP)

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
