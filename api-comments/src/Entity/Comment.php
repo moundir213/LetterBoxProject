c7ffb714150a607463d9983294fb4e6b5d7cc33b
ayoug <ayoub.essalhia@gmail.com>
2025-02-12 23:46:35 +0100

    feat: added fields and filters for api integration with main project

--- api-comments/src/Entity/Comment.php
+++ api-comments/src/Entity/Comment.php
@@ -2,11 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\CommentRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ApiResource(
@@ -25,21 +28,28 @@ class Comment
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank]
     #[Groups(["comment:read", "comment:write"])]
-    private ?string $author = null;
+    #[ApiFilter(SearchFilter::class, strategy: 'exact')]
+    private ?int $author = null;
+
+    #[ORM\Column]
+    #[Assert\NotBlank]
+    #[Groups(["comment:read", "comment:write"])]
+    #[ApiFilter(SearchFilter::class, strategy: 'exact')]
+    private ?int $anime = null;
 
     #[ORM\Column(type: Types::TEXT)]
     #[Assert\NotBlank]
     #[Groups(["comment:read", "comment:write"])]
     private ?string $content = null;
 
-    #[ORM\Column]
+    #[ORM\Column(name: "created_at", type: Types::DATETIME_IMMUTABLE)]
+    #[SerializedName("created_at")]
     #[Groups(["comment:read"])]
-    private ?\DateTimeImmutable $created_at = null;
-
+    private ?\DateTimeImmutable $createdAt = null;
 
     public function __construct()
     {
-        $this->created_at = new \DateTimeImmutable();
+        $this->createdAt = new \DateTimeImmutable();
     }
 
     public function getId(): ?int
@@ -47,12 +57,12 @@ class Comment
         return $this->id;
     }
 
-    public function getAuthor(): ?string
+    public function getAuthor(): ?int
     {
         return $this->author;
     }
 
-    public function setAuthor(string $author): static
+    public function setAuthor(int $author): static
     {
         $this->author = $author;
         return $this;
@@ -71,11 +81,23 @@ class Comment
 
     public function getCreatedAt(): ?\DateTimeImmutable
     {
-        return $this->created_at;
+        return $this->createdAt;
     }
 
-    public function setCreatedAt(?\DateTimeImmutable $created_at): void
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): void
     {
-        $this->created_at = $created_at;
+        $this->createdAt = $createdAt;
+    }
+
+    public function getAnime(): ?int
+    {
+        return $this->anime;
+    }
+
+    public function setAnime(int $anime): static
+    {
+        $this->anime = $anime;
+
+        return $this;
     }
 }
